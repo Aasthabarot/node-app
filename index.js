5e255ca6fe5d4d97300ecd71ecb725974d691677
Aasthabarot <barotaastha38@gmail.com>
2024-09-05 15:13:35 +0530

    save

--- index.js
+++ index.js
@@ -6,38 +6,64 @@ const index = fs.readFileSync('index.html', 'utf-8');
 // Load data.json once when the server starts
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 // Assuming you want to use the first product in some way
-const product = data.product[4];
+const product = data.product;
 
-const server = http.createServer((req, res) => {
-    switch(req.url) {
-        case '/hello':
-            // Serve the index.html
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(index);
-            break;
 
-        case '/demo':
-            // Serve the data.json content as it is
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(data));
-            break;
+const morgan = require('morgan')
+const express = require('express');
+const { type } = require('os');
+const server = express()
 
-        case '/product':
-            // Serve only the first product as JSON
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            const dynamic = index.replace('***name***',product.name).replace("**bio**",product.bio)
-            res.end(dynamic);
-            break;
+server.use(morgan('default'));
 
-        default:
-            // Handle 404 Not Found
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('404 Not Found');
-            break;
+// server.use((req,res,next)=>
+// {
+//    console.log(new Date(),req.hostname ,req.method, req.ip ,req.get('user-agent'))
+//    next()
+// })
+
+const auth = ((req , res , next)=>
+{
+    console.log(req.query)
+    if(req.query.password=='123')
+    {
+          next()
     }
-    console.log(`Request received: ${req.url}`);
-});
+    else
+    {
+      res.sendStatus(401)
+    }
+})
 
-server.listen(3030, () => {
-    console.log("Server is listening on port 3030");
-});
+server.use(express.static('public'));
+server.use(express.json());
+// server.use(auth);
+server.get('/', auth,(req,res) => {
+   
+    res.json({type:'GET1'})
+})
+server.get('/',(req,res)=>{
+    res.json({type:'GET2'})
+})
+server.put('/',(req,res)=>{
+    res.json({type:'PUT'})
+})
+server.post('/',(req,res)=>{
+    res.json({type:'POST'})
+})
+server.delete('/',(req,res)=>{
+    res.json({type:'DELETE'})
+})
+server.patch('/',(req,res)=>{
+    res.json({type:'PATCH'})
+})
+
+// server.get('/',(req,res)=>{
+//     //   res.send("hi from aastha")
+//       res.json(product)
+// })
+
+
+server.listen(3030,()=>{
+    console.log("server started")
+})
